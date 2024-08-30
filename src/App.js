a9eecae1f6220b70d81fc13d666f9155db35d5f5
Chantal  Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2024-08-30 07:21:07 +0200

    Updated conflict App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import Telemedicine from './js/Telemedicine';
 // import SignupPage from './js/SignupPage';
 // import LoginPage from './js/LoginPage';
 // import LandingPage from './js/LandingPage';
-<<<<<<< HEAD
-import AppointmentPage from './js/Appointment';
-=======
 // import AppointmentPage from './js/Appointment';
->>>>>>> 45d913f713fe929688c3b8426643b1b47758f966
 
 import './App.css';
 
@@ -24,11 +20,7 @@ function App() {
         {/* <Route path="/" element={<SignupPage />} /> */}
         {/* <Route path="/" element={<LoginPage />} /> */}
         {/* <Route path="/" element={<LandingPage />} /> */}
-<<<<<<< HEAD
-        <Route path="/" element={<AppointmentPage />} />
-=======
         {/* <Route path="/" element={<AppointmentPage />} /> */}
->>>>>>> 45d913f713fe929688c3b8426643b1b47758f966
       </Routes>
     </Router>
     
