86e22390c9607d4300fb51e0df2113cad3103bc3
Bonolo <herminahbonolo01@gmail.com>
2024-08-31 14:38:27 +0200

    phone frames are all done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Telemedicine from './js/Telemedicine';
-import Settings from './js/Settings';
+// import Settings from './js/Settings';
 // import WelcomePage from './js/WelcomePage';
 // import SignupPage from './js/SignupPage';
 // import LoginPage from './js/LoginPage';
 // import LandingPage from './js/LandingPage';
-// import AppointmentPage from './js/Appointment';
+import AppointmentPage from './js/Appointment';
 
 import './App.css';
 
@@ -14,13 +14,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Settings />} />
+        {/* <Route path="/" element={<Settings />} /> */}
         {/* <Route path='/'element={<Telemedicine/>} /> */}
         {/* <Route path="/" element={<WelcomePage />} /> */}
         {/* <Route path="/" element={<SignupPage />} /> */}
         {/* <Route path="/" element={<LoginPage />} /> */}
         {/* <Route path="/" element={<LandingPage />} /> */}
-        {/* <Route path="/" element={<AppointmentPage />} /> */}/
+        <Route path="/" element={<AppointmentPage />} />
       </Routes>
     </Router>
     
--- src/js/LoginPage.js
+++ src/js/LoginPage.js
@@ -5,33 +5,35 @@ import { MdEmail, MdPerson, MdLock } from 'react-icons/md';
 
 const Login = () => {
     return (
-        <div className="login-page">
-            <div className="header">
-                <h1>Login</h1>
-            </div>
-            <div className="welcome-image">
-                <img src={welcomeImage} alt="Welcome" />
-            </div>
-            <div className="blue-container">
-                <form>
-                    <div className="input-group">
-                        <MdEmail className="input-icon" />
-                        <span>|</span>
-                        <input type="email" placeholder="Email" required />
-                    </div>
-                    <div className="input-group">
-                        <MdPerson className="input-icon" />
-                        <span>|</span>
-                        <input type="text" placeholder="Username" required />
-                    </div>
-                    <div className="input-group">
-                        <MdLock className="input-icon" />
-                        <span>|</span>
-                        <input type="password" placeholder="Password" required />
-                    </div>
-                    <button type="submit">Create an Account</button>
-                    <p className="login-link">Already have an account? <a href="/login">Login</a></p>
-                </form>
+        <div className="phone-screen">
+            <div className="signup-page">
+                <div className="header">
+                    <h1>Login</h1>
+                </div>
+                <div className="welcome-image">
+                    <img src={welcomeImage} alt="Welcome" />
+                </div>
+                <div className="blue-container">
+                    <form>
+                        <div className="input-group">
+                            <MdEmail className="input-icon" />
+                            <span>|</span>
+                            <input type="email" placeholder="Email" required />
+                        </div>
+                        <div className="input-group">
+                            <MdPerson className="input-icon" />
+                            <span>|</span>
+                            <input type="text" placeholder="Username" required />
+                        </div>
+                        <div className="input-group">
+                            <MdLock className="input-icon" />
+                            <span>|</span>
+                            <input type="password" placeholder="Password" required />
+                        </div>
+                        <button type="submit">Create an Account</button>
+                        <p className="login-link">Already have an account? <a href="/login">Login</a></p>
+                    </form>
+                </div>
             </div>
         </div>
     );
